91ff31ff8f5c6d48a8d8c308a98b929e4386e9b3
brettthompson <thompbre2017@bcschools.net>
2016-06-03 00:06:49 -0400

    added camera coding (VIEWPORT method)

--- core/src/com/packtpub/libgdx/bludbourne/screens/MainGameScreen.java
+++ core/src/com/packtpub/libgdx/bludbourne/screens/MainGameScreen.java
@@ -1,11 +1,8 @@
 /**
  * Created by brett on 6/1/2016.
  */
-<<<<<<< HEAD
-package com.packtpub.libgdx.bludbourne.screen
-=======
+
 package com.packtpub.libgdx.bludbourne.screens;
->>>>>>> origin/develop
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
@@ -98,7 +95,7 @@ public class MainGameScreen implements Screen {
 
         updatePortalLayerActivation(_player.boundingBox);
 
-        if( !isCollisionWithMapLayer(_player.boundingBox) ){
+        if (!isCollisionWithMapLayer(_player.boundingBox)) {
             _player.setNextPositionToCurrent();
         }
         _controller.update(delta);
@@ -107,7 +104,71 @@ public class MainGameScreen implements Screen {
         _mapRenderer.render();
 
         _mapRenderer.getBatch().begin();
-        _mapRenderer.getBatch().draw(_currentPlayerFrame, _currentPlayerSprite.getX(), _currentPlayerSprite.getY(), 1,1);
+        _mapRenderer.getBatch().draw(_currentPlayerFrame, _currentPlayerSprite.getX(), _currentPlayerSprite.getY(), 1, 1);
         _mapRenderer.getBatch().end();
     }
+
+        @Override
+                public void resize(int width, int height){
+        }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+        _player.dispose();
+        _controller.dispose();
+        Gdx.input.setInputProcessor(null);
+        _mapRenderer.dispose();
+    }
+
+private void setupViewport(int width, int height){
+    // make the viewport a percentage of the total display area
+    VIEWPORT.virtualWidth = width;
+    VIEWPORT.virtualHeight = height;
+
+    // current dimensions
+    VIEWPORT.virtualWidth = VIEWPORT.virtualWidth;
+    VIEWPORT.virtualHeight = VIEWPORT.virtualHeight;
+
+    //pixel dimensions of display
+    VIEWPORT.physicalWidth = Gdx.graphics.getWidth();
+    VIEWPORT.physicalHeight = Gdx.graphics.getHeight();
+
+    //aspect ratio for current viewport
+    VIEWPORT.aspectRatio = (VIEWPORT.virtualWidth / VIEWPORT.virtualHeight);
+
+    //update viewport if there could be skewing
+    if( VIEWPORT.physicalWidth / VIEWPORT.virtualHeight >= VIEWPORT.aspectRatio) {
+
+        //letter box left and right
+        VIEWPORT.viewportWidth = VIEWPORT.viewportHeight *
+                (VIEWPORT.physicalWidth / VIEWPORT.physicalHeight);
+        VIEWPORT.viewportHeight = VIEWPORT.virtualHeight;
+    }else{
+    //letterbox above and below
+        VIEWPORT.viewportWidth = VIEWPORT.virtualWidth;
+        VIEWPORT.viewportHeight = VIEWPORT.viewportWidth *
+                (VIEWPORT.physicalHeight/VIEWPORT.physicalWidth);
+    }
+
+    Gdx.app.debug(TAG, "WorldRenderer: virtual: (" +
+    VIEWPORT.virtualWidth + "," + VIEWPORT.virtualHeight + ")" );
+
+    Gdx.app.debug(TAG, "WorldRenderer: viewport: (" +
+    VIEWPORT.viewportWidth + "," + VIEWPORT.viewportHeight + ")");
+
+    Gdx.app.debug(TAG, "WorldRender: physical: (" +
+    VIEWPORT.physicalWidth + "," + VIEWPORT.physicalHeight + ")");
+    }
 }
+
+
+
+
