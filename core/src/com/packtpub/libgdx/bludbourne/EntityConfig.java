85796322a43239662afa2e1899b785455dbf69fb
brettthompson <thompbre2017@bcschools.net>
2016-06-06 17:56:01 -0400

    added code. stuck on making new Entity class

---
+++ core/src/com/packtpub/libgdx/bludbourne/EntityConfig.java
@@ -0,0 +1,5 @@
+/**
+ * Created by brett on 6/6/2016.
+ */
+public class EntityConfig {
+}
---
+++ core/src/com/packtpub/libgdx/bludbourne/EntityFactory.java
@@ -0,0 +1,44 @@
+/**
+ * Created by brett on 6/6/2016.
+ */
+package com.packptub.libgdx.bludbourne;
+
+import com.badlogic.gdx.utils.Json;
+
+public class EntityFactory {
+
+    private static Json _json = new Json();
+
+    public static enum EntityType{
+        PLAYER,
+        DEMO_PLAYER,
+        NPC
+    }
+
+    public static String PLAYER_CONFIG = "scripts/player.json";
+
+    static public Entity getEntity(EntityType entityType){
+        Entity entity = null;
+        switch(entityType) {
+            case PLAYER:
+                entity = new Entity(
+                        new PlayerInputComponent(),
+                        new PlayerPhysicsComponent(),
+                        new PlayerGraphicsComponent());
+                    entity.setEntityConfig(
+                            Entity.getEntityConfig(
+                                    EntityFactory.PLAYER_CONFIG));
+                entity.sendMessage(
+                        Component.MESSAGE.LOAD_ANIMATIONS, _json.toJson(entity.getEntityConfig()));
+                return entity;
+            case NPC:
+                entity = new Entity(
+                        new NPCInputComponent(),
+                        new NPCPhysicsComponent(),
+                        new NPCGraphicsComponent());
+                    return entity;
+            default:
+                return null;
+        }
+    }
+}
